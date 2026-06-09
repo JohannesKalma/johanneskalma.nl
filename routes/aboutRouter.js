85841105e9c6ca9cb4cfdf86a25bbafc96eb591c
johannes <johannes@oarsnet.nl>
2026-06-09 15:32:23 +0200

    cleanup

--- routes/aboutRouter.js
+++ routes/aboutRouter.js
@@ -8,7 +8,6 @@ router.get('/', async (req, res, next) => {
     res.render('about', {
         footer: await footer()
     });
-    //res.send(`Taxonomy route is working for ${filterType} with slug: ${slug}`);
 })
 
 export default router;
